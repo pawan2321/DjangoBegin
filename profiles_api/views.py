5b6b95865b83fc8176b3ce3448543a83e3951691
pawan2321 <pawan2321@gmail.com>
2020-01-23 20:27:49 +0530

    Added Login Authentication in User Profiles

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from profiles_api import serializers, models, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -104,3 +106,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name", "email",)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """
+    Handle Authentication
+    """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
